455d49e8d249029acb438b3de7aedc8f37919292
SLMT <sam123456777@gmail.com>
2019-10-06 15:35:29 +0800

    Add class 3

---
+++ class-1-10/Class3/Class3/Program.cs
@@ -0,0 +1,19 @@
+﻿// 程式庫
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class3 // 自己命名的程式庫，像是一間公司
+{
+    class Program // 負責程式庫中一個工作的元件，像是公司的部門
+    {
+        static void Main(string[] args) // 一個工作之中更細部的工作，像是公司部門內的小工作
+        {
+            // 在你的視窗上顯示右列文字
+            Console.WriteLine("Hello World! 歡迎學習 C#");
+            Console.ReadKey();
+        }
+    }
+}
