eb51950df1fe14bb28e50a8692ed062b2097eba6
守真 孫 <oscarsun72@hotmail.com>
2020-07-26 21:46:31 +0800

    Class42 OK

---
+++ class-40-50/Class42/Class42/BlackPiece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class42
+{
+    public class BlackPiece:Piece
+    {
+        public BlackPiece(int x,int y) : base(x, y)
+        {
+            Image = Properties.Resources.black;
+        }
+    }
+}
---
+++ class-40-50/Class42/Class42/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class42
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //表單背景圖片
+            BackgroundImage = Properties.Resources.board;
+            ////掛上表單事件連結
+            //Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            //設定表單長寬為棋盤圖檔的長寬
+            Height = Properties.Resources.board.Height;
+            Width = Properties.Resources.board.Width;
+            //設定表單出現在螢幕的正中央
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //當對表單（視窗）按下Esc鍵時，關閉表單
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+        }
+
+        private bool isBlack = true;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isBlack)
+            {
+                Controls.Add(new BlackPiece(e.X, e.Y));
+                isBlack = false;
+            }
+            else if (!isBlack)
+            {
+                Controls.Add(new WhitePiece(e.X, e.Y));
+                isBlack = true;
+            }
+
+        }
+
+    }
+
+}
