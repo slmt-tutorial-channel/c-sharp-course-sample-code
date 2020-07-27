647e7eea9fe9eb8846533ddca504830a2b519f24
守真 孫 <oscarsun72@hotmail.com>
2020-07-27 21:55:03 +0800

    OK

---
+++ class-40-50/Class45/Class45/BlackPiece.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Class45
+{
+    public class BlackPiece:Piece
+    {
+        public BlackPiece(int x,int y) : base(x, y)
+        {
+            Image = Properties.Resources.black;
+        }
+        public BlackPiece(Point point) : base(point)
+        {
+            Image = Properties.Resources.black;
+        }
+        internal override PieceType GetPieceTyep()
+        {
+            return PieceType.BLACK;
+        }
+    }
+}
---
+++ class-40-50/Class45/Class45/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class45
+{
+    public class Game
+    {
+        private static PieceType currentPlayer = PieceType.BLACK;
+
+        //要給form1呼叫的
+        internal static Piece PlaceAPiece(int x, int y)
+        {
+            Piece piece = Board.PlaceAPiece(x, y, currentPlayer);
+            if (piece != null)
+            {
+                if (currentPlayer == PieceType.BLACK)
+                    currentPlayer = PieceType.WHITE;
+                else if (currentPlayer == PieceType.WHITE)
+                    currentPlayer = PieceType.BLACK;
+            }
+            return piece;
+        }
+
+        //要給form1呼叫的
+        internal static bool CanBePlaced(int x, int y)
+        {
+            return Board.CanBePlaced(x, y);
+        }
+
+
+        private static void CheckWiner()
+        {
+
+        }
+
+    }
+}
