35142822e1fd72a85d4cfacc8cd5ce9e00b5082b
守真 孫 <oscarsun72@hotmail.com>
2020-07-29 00:54:09 +0800

    在有人快勝利時提出警告訊息

---
+++ class-40-50/Class46Ex_3/Class46Ex_3/PieceType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class46Ex_3
+{
+    public enum PieceType
+    {
+        BLACK, WHITE,NONE
+    }
+}
