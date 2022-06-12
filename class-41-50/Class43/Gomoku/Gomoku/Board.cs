d080f490a04aab8a02a71f1914725f3f8e0347bc
SLMT <sam123456777@gmail.com>
2022-06-12 22:33:57 +0800

    Add class 43

---
+++ class-41-50/Class43/Gomoku/Gomoku/Board.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Gomoku
+{
+    class Board
+    {
+        private static readonly Point NO_MATCH_NODE = new Point(-1, -1);
+
+        private static readonly int OFFSET = 75;
+        private static readonly int NODE_RADIUS = 10;
+        private static readonly int NODE_DISTANCE = 75;
+
+        public bool CanBePlaced(int x, int y)
+        {
+            // 找出最近的節點 (Node)
+            Point nodeId = FindTheClosetNode(x, y);
+
+            // 如果沒有的話，回傳 false
+            if (nodeId == NO_MATCH_NODE)
+                return false;
+
+            // TODO: 如果有的話，檢查是否已經棋子存在
+
+            return true;
+        }
+
+        private Point FindTheClosetNode(int x, int y)
+        {
+            int nodeIdX = FindTheClosetNode(x);
+            if (nodeIdX == -1)
+                return NO_MATCH_NODE;
+
+            int nodeIdY = FindTheClosetNode(y);
+            if (nodeIdY == -1)
+                return NO_MATCH_NODE;
+
+            return new Point(nodeIdY, nodeIdY);
+        }
+
+        private int FindTheClosetNode(int pos)
+        {
+            if (pos < OFFSET - NODE_RADIUS)
+                return -1;
+
+            pos -= OFFSET;
+
+            int quotient = pos / NODE_DISTANCE;
+            int remainder = pos % NODE_DISTANCE;
+
+            if (remainder <= NODE_RADIUS)
+                return quotient;
+            else if (remainder >= NODE_DISTANCE - NODE_RADIUS)
+                return quotient + 1;
+            else
+                return -1;
+        }
+    }
+}
---
+++ class-41-50/Class43/Gomoku/Gomoku/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    public partial class Form1 : Form
+    {
+        private Board board = new Board();
+
+        private bool isBlack = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (isBlack)
+            {
+                this.Controls.Add(new BlackPiece(e.X, e.Y));
+                isBlack = false;
+            } else
+            {
+                this.Controls.Add(new WhitePiece(e.X, e.Y));
+                isBlack = true;
+            }
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (board.CanBePlaced(e.X, e.Y))
+            {
+                this.Cursor = Cursors.Hand;
+            } else
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+    }
+}
